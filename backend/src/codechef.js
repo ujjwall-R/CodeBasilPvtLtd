cdad71dad0f084a48d76888d0a612e34f87f8e80
Ujjwal R <ujjwal.raj.5798@gmail.com>
2022-06-04 12:28:58 +0530

    heroku ready

--- backend/src/codechef.js
+++ backend/src/codechef.js
@@ -2,7 +2,12 @@ import cheerio from "cheerio";
 import puppeteer from "puppeteer";
 
 const getRawData = async (un) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
   const page = await browser.newPage();
 
   await page.goto(`https://www.codechef.com/users/${un}`, {
