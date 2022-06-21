17740223dca7107bd51b7ef30119569923ba3ada
Ujjwal R <75781631+ujjwall-R@users.noreply.github.com>
2022-06-21 14:08:42 +0530

    Update mongoose.js

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
       const con = await mongoose.connect(
-        // "mongodb+srv://ujjwalraj5798:ujjwalraj5798@cluster0.wbish.mongodb.net/binay-api?retryWrites=true&w=majority",
-        // "mongodb+srv://BinayBhattacharya:Binaycodes12@cluster0.rfdd7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-        "mongodb+srv://codebasilPvtTech:codebasil1234@cluster0.ugqd4.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb://localhost:27017/codebasil-api",
         {
           useUnifiedTopology: true,
           useNewUrlParser: true,
